39bf63145d6840890c5ec78d7047dcbcfadd5664
Bita Djaghouri <bita@bitadj.com>
2015-04-11 09:21:02 -0700

    getting paid

---
+++ 03-git-methods-interpolation-and-conditionals/getting_paid.rb
@@ -0,0 +1,35 @@
+getting_paid = false
+balance = 11
+borrowed_money_mom = 1500
+string = "Django"
+#flow control
+
+if string.downcase == "django" && balance >= 11
+	puts "buy a dress!"
+	puts string
+end
+
+# if getting_paid 
+# 	puts "Go buy shoes"
+# elsif balance > borrowed_money_mom
+# 	puts "buy shoes at Ross"
+# 	if borrowed_money_mom
+# 		puts "Go to bloomingdales"
+# 	end
+# elsif borrowed_money_mom
+# 	puts "Go to bloomingdales"
+# else
+# 	puts "Go buy ramen"
+# end
+
+# if getting_paid 
+# 	puts "Go buy shoes"
+# elsif balance > 10
+# 	puts "buy shoes at Ross"
+# end
+
+# if borrowed_money_mom
+# 	puts "Go to bloomingdales"
+# else
+# 	puts "Go buy ramen"
+# end
